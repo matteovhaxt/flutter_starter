50568f6e3477ad2967f1feb0a247854d9265d077
Matteo von Haxthausen <matteovhaxt@gmail.com>
2024-03-04 14:01:08 +0100

    feat: logger provider

--- lib/providers.dart
+++ lib/providers.dart
@@ -1,3 +1,5 @@
+// Package imports:
+import 'package:logger/web.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
@@ -6,4 +8,9 @@ part 'providers.g.dart';
 @Riverpod(keepAlive: true)
 SupabaseClient supabase(SupabaseRef ref) {
   return Supabase.instance.client;
+}
+
+@riverpod
+Logger logger(LoggerRef ref) {
+  return Logger();
 }
