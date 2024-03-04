2540de9ab89f54ccd6456206f1a9a98d74901235
Matteo von Haxthausen <matteovhaxt@gmail.com>
2024-03-04 15:20:46 +0100

    feat: sign out button

--- lib/app/features/navigation/view.dart
+++ lib/app/features/navigation/view.dart
@@ -3,17 +3,25 @@ import 'package:flutter/material.dart';
 
 // Package imports:
 import 'package:go_router/go_router.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:lucide_icons/lucide_icons.dart';
 
-class NavigationView extends StatelessWidget {
+// Project imports:
+import '../features.dart';
+
+class NavigationView extends ConsumerWidget {
   const NavigationView(this.navigationShell, {super.key});
 
   final StatefulNavigationShell navigationShell;
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
       body: navigationShell,
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(LucideIcons.logOut),
+        onPressed: () => ref.read(authStateProvider.notifier).signOut(),
+      ),
       bottomNavigationBar: NavigationBar(
         selectedIndex: navigationShell.currentIndex,
         destinations: const [
