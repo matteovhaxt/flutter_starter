c9023102a1f66c05578974dfa1e7745a72c87ac1
Matteo von Haxthausen <matteovhaxt@gmail.com>
2024-04-01 08:44:17 +0200

    fix: stateful home view

--- lib/app/features/home/view.dart
+++ lib/app/features/home/view.dart
@@ -1,17 +1,8 @@
 // Flutter imports:
 import 'package:flutter/material.dart';
 
-class HomeView extends StatefulWidget {
+class HomeView extends StatelessWidget {
   const HomeView({super.key});
-
-  @override
-  State<HomeView> createState() => _HomeViewState();
-}
-
-class _HomeViewState extends State<HomeView> {
-  int selectedIndex = 0;
-
-  void onSelectIndex(int index) => setState(() => selectedIndex = index);
 
   @override
   Widget build(BuildContext context) {
