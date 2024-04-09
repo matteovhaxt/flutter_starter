454c7fff0f0f4a5f4e90b4cedb6393e85fd47a5d
Matteo von Haxthausen <matteovhaxt@gmail.com>
2024-04-09 10:48:50 +0200

    move app startup provider to utils

--- lib/app/core/providers/providers.dart
+++ lib/app/core/providers/providers.dart
@@ -1,5 +1,4 @@
 export 'user.dart';
-export 'app_startup.dart';
 export 'clients.dart';
 export 'utils.dart';
 export 'router.dart';
--- lib/app/core/providers/utils.dart
+++ lib/app/core/providers/utils.dart
@@ -1,10 +1,27 @@
 // Package imports:
+import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter/material.dart';
 import 'package:logger/logger.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+import '../../../env.dart';
 
 part 'utils.g.dart';
 
 @riverpod
 Logger logger(LoggerRef ref) {
   return Logger();
+}
+
+@Riverpod(keepAlive: true)
+Future<void> appStartup(AppStartupRef ref) async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await EasyLocalization.ensureInitialized();
+
+  await Supabase.initialize(
+    url: Env.supabaseApiUrl,
+    anonKey: Env.supabaseAnonKey,
+  );
 }
